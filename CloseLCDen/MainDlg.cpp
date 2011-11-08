c27a96cce2121aeac7a6ae061fa9ff41c0e29ca9
Snger <sjs.stef@gmail.com>
2011-11-08 10:50:01 +0800

    Code

---
+++ CloseLCDen/MainDlg.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include <windows.h>
+#include <windowsx.h>
+#include "resource.h"
+#include "MainDlg.h"
+
+
+BOOL WINAPI Main_Proc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch(uMsg)
+    {
+        HANDLE_MSG(hWnd, WM_INITDIALOG, Main_OnInitDialog);
+        HANDLE_MSG(hWnd, WM_COMMAND, Main_OnCommand);
+		HANDLE_MSG(hWnd,WM_CLOSE, Main_OnClose);
+    }
+
+    return FALSE;
+}
+
+BOOL Main_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+{
+    return TRUE;
+}
+
+void Main_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+    switch(id)
+    {
+        case IDC_OK:
+		{
+			SendMessage(HWND_BROADCAST,WM_SYSCOMMAND,SC_MONITORPOWER,2);//Turn off Laptop LCD Screen
+		}
+        break;
+        default:
+		break;
+    }
+}
+
+void Main_OnClose(HWND hwnd)
+{
+    EndDialog(hwnd, 0);
+}
